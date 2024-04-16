8596b3dc5ebb15952e6969d65511348a623df10b
Daniel Jauch <daniel.p.jauch@gmail.com>
2024-04-16 16:18:56 -0600

    Add region arg for flexibility

--- lib/example/api.ex
+++ lib/example/api.ex
@@ -7,40 +7,48 @@ defmodule Example.Api do
 
   plug Tesla.Middleware.JSON
 
-  @spec get_summoner_by_name(name :: String.t()) :: Tesla.Env.result()
-  def get_summoner_by_name(name) do
-    "na1"
+  @spec get_summoner_by_name(name :: String.t(), region :: String.t()) ::
+          Tesla.Env.result()
+  def get_summoner_by_name(name, region \\ "na1") do
+    region
     |> client()
     |> get("/lol/summoner/v4/summoners/by-name/#{name}")
   end
 
-  @spec get_summoner_by_puuid(puuid :: String.t()) :: Tesla.Env.result()
-  def get_summoner_by_puuid(puuid) do
-    "na1"
+  @spec get_summoner_by_puuid(puuid :: String.t(), region :: String.t()) ::
+          Tesla.Env.result()
+  def get_summoner_by_puuid(puuid, region \\ "na1") do
+    region
     |> client()
     |> get("/lol/summoner/v4/summoners/by-puuid/#{puuid}")
   end
 
-  @spec get_account_by_puuid(puuid :: String.t()) :: Tesla.Env.result()
-  def get_account_by_puuid(puuid) do
-    "americas"
+  @spec get_account_by_puuid(puuid :: String.t(), region :: String.t()) ::
+          Tesla.Env.result()
+  def get_account_by_puuid(puuid, region \\ "americas") do
+    region
     |> client()
     |> get("/riot/account/v1/accounts/by-puuid/#{puuid}")
   end
 
-  @spec get_matches_by_puuid(puuid :: String.t(), count :: integer()) ::
+  @spec get_matches_by_puuid(
+          puuid :: String.t(),
+          count :: integer(),
+          region :: String.t()
+        ) ::
           Tesla.Env.result()
-  def get_matches_by_puuid(puuid, count \\ 5) do
-    "americas"
+  def get_matches_by_puuid(puuid, count \\ 5, region \\ "americas") do
+    region
     |> client()
     |> get(
       "/lol/match/v5/matches/by-puuid/#{puuid}/ids?count=#{Integer.to_string(count)}"
     )
   end
 
-  @spec get_match_details(match_id :: String.t()) :: Tesla.Env.result()
-  def get_match_details(match_id) do
-    "americas"
+  @spec get_match_details(match_id :: String.t(), region :: String.t()) ::
+          Tesla.Env.result()
+  def get_match_details(match_id, region \\ "americas") do
+    region
     |> client()
     |> get("/lol/match/v5/matches/#{match_id}")
   end
